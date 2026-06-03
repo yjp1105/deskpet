1b4f95eb1beb708861ee3ed854647a81f9cb3004
yjp1105 <kongxinshigao@126.com>
2026-06-03 20:30:19 +0800

    Add installation script for environment setup

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001
+echo 正在为您配置富冈义勇桌宠运行环境...
+echo 正在安装 PyQt5 界面库和 requests 网络库...
+pip install pyqt5 requests -i https://pypi.tuna.tsinghua.edu.cn/simple
+echo ---------------------------------------
+echo 环境安装完成！请按任意键退出。
+pause
